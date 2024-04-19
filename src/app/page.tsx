70c22e384723a39dec3cec4bd7baace60e695290
Rafael Pavanelli <rafaelzica86@hotmail.com>
2024-04-19 14:02:54 -0300

    scrool-porcentage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Courses } from "@/components/courses";
 import { Jobs } from "@/components/jobs";
 import { Languages } from "@/components/languages";
@@ -5,11 +6,17 @@ import { Links } from "@/components/links";
 import { Main } from "@/components/main";
 import { Projects } from "@/components/projects";
 import { Skills } from "@/components/skills";
-
+import { motion, useScroll } from "framer-motion";
 export default function Home(){
+    const { scrollYProgress } = useScroll();
+
     {
         return(
             <div className="bg-[#F3F8F4] max-h-screen ">
+                 <motion.div
+        className="fixed top-0 left-0 right-0 h-2 bg-[#4a7265] transform-0 z-10"
+        style={{ scaleX: scrollYProgress }}
+      />
             <Main />
             <div className="px-10">
             <Courses />
