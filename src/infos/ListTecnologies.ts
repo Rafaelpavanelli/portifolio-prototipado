8803ce6632f169b2bfb3854f3af372b181057bb6
Rafael Pavanelli <rafaelzica86@hotmail.com>
2024-07-21 05:27:15 -0300

    fix/fix new UI

---
+++ src/infos/ListTecnologies.ts
@@ -0,0 +1,51 @@
+type TypeTecnologies = {
+  name: string;
+  icon: string;
+};
+
+export const Tecnologies: TypeTecnologies[] = [
+  {
+    name: "tailwind",
+    icon: "/TecnologieIcon/tailwind.png",
+  },
+  {
+    name: "javascript",
+    icon: "/TecnologieIcon/javascript.png",
+  },
+  {
+    name: "next",
+    icon: "/TecnologieIcon/next.png",
+  },
+  {
+    name: 'expo',
+     icon: '/TecnologieIcon/expo.png'
+  },
+  {
+    name: 'react',
+     icon: '/TecnologieIcon/react.png'
+  },
+  {
+    name: 'html',
+     icon: '/TecnologieIcon/html.png'
+  },
+  {
+    name:'css',
+     icon: '/TecnologieIcon/css.png'
+  },
+  {
+    name: 'express',
+     icon: '/TecnologieIcon/express.png'
+  },
+  {
+    name: 'sass',
+     icon: '/TecnologieIcon/sass.png'
+  },
+  {
+    name: 'jquery',
+     icon: '/TecnologieIcon/jquery.png'
+  },
+  {
+    name: 'firebase',
+     icon: '/TecnologieIcon/firebase.png'
+  }
+];
